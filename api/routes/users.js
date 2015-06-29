34e045d6446268955c9ba630fbbb195b93e86ab7
sachdevs <sachdev.96@gmail.com>
2015-06-29 16:44:46 -0400

    crud for users

--- api/routes/users.js
+++ api/routes/users.js
@@ -4,7 +4,8 @@ var User = require('../models/user');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
+
+function listUsers() {
     User.find({}, function(err, users) {
         var userMap = {};
 
@@ -14,6 +15,23 @@ router.get('/', function(req, res) {
 
         res.json(userMap);
     });
+}
+
+function findById(id) {
+    User.findById(id, function(err, obj) {
+        if (err)
+            return err;
+        else
+            return obj;
+    });
+}
+
+router.get('/:userId', function(req, res) {
+	res.json(findById(req.params.userId));
+});
+
+router.get('/', function(req, res) {
+    res.json(listUsers());
 });
 
 router.post('/', function(req, res) {
@@ -27,6 +45,27 @@ router.post('/', function(req, res) {
         else
             res.json(req.body);
     });
+});
+
+router.put('/:userId', function(req, res) {
+    var user = findById(req.params.userId);
+    user.userId = req.body.userId;
+    user.name = req.body.name;
+    user.genreList = req.body.genreList;
+    user.save(function(err) {
+        if (err)
+            res.send(err);
+        else
+            res.json(req.body);
+    });
+});
+
+router.delete('/:userId', function(req, res){
+	User.findByIdAndRemove(req.params.userId, function(err){
+		if(err)
+			res.send(err);
+		res.json(msg: 'User deleted');
+	});
 });
 
 module.exports = router;
