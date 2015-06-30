aa92b7ae9b4af854e815a3033b7764f1ab9a6fde
sachdevs <sachdev.96@gmail.com>
2015-06-30 15:04:06 -0400

    finished user api endpoint

--- api/routes/users.js
+++ api/routes/users.js
@@ -20,19 +20,17 @@ function listUsers(done) {
     });
 }
 
-function findById(id, done) {
-    User.find({
-        userId: id
-    }).exec(function(err, user) {
-        if (err)
-            return err;
-        return done(user[0]);
-    });
-}
-
 router.get('/:userId', function(req, res) {
-    findById(req.params.userId, function(obj) {
-        res.json(obj);
+    User.find({
+        userId: req.params.userId
+    }).exec(function(err, users) {
+        if (err)
+            return res.send(err);
+        else if (users.length === 0)
+            res.status(404).json({
+                msg: 'Not found'
+            });
+        res.json(users[0]);
     });
 });
 
@@ -49,38 +47,52 @@ router.post('/', function(req, res) {
     user.genreList = req.body.genreList;
     user.save(function(err) {
         if (err)
-            res.send(err);
+            return res.send(err);
         else
             res.json(req.body);
     });
 });
 
 router.put('/:userId', function(req, res) {
-    findById(req.params.userId, function(user) {
+    User.find({
+        userId: req.params.userId
+    }).exec(function(err, users) {
+        if (err)
+            return res.send(err);
+        else if (users.length === 0) {
+            return res.status(404).json({
+                msg: 'Not found'
+            });
+        }
+        var user = users[0];
         user.userId = req.body.userId;
         user.name = req.body.name;
         user.genreList = req.body.genreList;
         user.save(function(err) {
             if (err)
-                res.send(err);
+                return res.send(err);
             else
-                res.json(req.body);
+                res.status(200).json(req.body);
         });
     });
 });
 
 router.delete('/:userId', function(req, res) {
-    findById(req.params.userId, function(obj) {
-        if(obj === undefined) {
-		   res.statusCode = 404;
-		   res.json({msg: 'Not found'});
-		}
-		console.log(obj);
+    User.find({
+        userId: req.params.userId
+    }).exec(function(err, users) {
+        if (err)
+            return res.send(err);
+        else if (users.length === 0) {
+            return res.status(404).json({
+                msg: 'Not found'
+            });
+        }
         User.remove({
             userId: req.params.userId
         }, function(err) {
             if (err)
-                res.send(err);
+                return res.send(err);
             res.json({
                 msg: 'User has been deleted'
             });
