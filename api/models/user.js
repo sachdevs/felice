3e01950455c258c4e3e497b4e29feff880ef61f1
sachdevs <sachdev.96@gmail.com>
2015-06-30 16:57:30 -0400

    The solution to all my problems

--- api/models/user.js
+++ api/models/user.js
@@ -2,16 +2,22 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    //Spotify user Id
+    //Spotify user Id: id
     userId: {
         type: String,
         index: {
             unique: true
         }
     },
-    //Spotify name
+    //Spotify name: (display_name)
     name: String,
+    email: String,//:email
+    spotifyURI: String,//:external_urls.spotify
+    //dp image url
+    imageUrl: String,//:images.0.url
+    country: String,//:country
     //Top ten genres
-    genreList: [String]
+    genreList: [String],
+    watchingList: [String]
 });
 module.exports = mongoose.model('User', UserSchema);
