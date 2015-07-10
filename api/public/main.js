b448fe50e45502ce55594581c132cae298824cb3
sachdevs <sachdev.96@gmail.com>
2015-07-10 15:11:02 -0400

    cleanup

--- api/public/main.js
+++ api/public/main.js
@@ -1,6 +1,7 @@
 //main logic goes here
+//REMEMBER TO MOVE THIS TO VIEW LOGIC
 $('.bs-sidebar').mouseenter(function(){
-	$(this).animate({width: '100px'}, 200);
+	$(this).animate({width: '150px'}, 200);
 });
 $('.bs-sidebar').mouseleave(function(){
 	$(this).animate({width: '40px'}, 200);
