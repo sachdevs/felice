ba92ff7126f731b109733300b7f7d2a85a7d46aa
sachdevs <sachdev.96@gmail.com>
2015-07-10 15:06:45 -0400

    sidebar animations and coloring

--- api/public/main.js
+++ api/public/main.js
@@ -1,1 +1,7 @@
 //main logic goes here
+$('.bs-sidebar').mouseenter(function(){
+	$(this).animate({width: '100px'}, 200);
+});
+$('.bs-sidebar').mouseleave(function(){
+	$(this).animate({width: '40px'}, 200);
+});
