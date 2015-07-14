9684c15e5ecaf85651a5a76fddbf9b97356c6eff
sachdevs <sachdev.96@gmail.com>
2015-07-14 10:41:33 -0400

    spotify auth logic without wrong files

--- api/public/main.js
+++ api/public/main.js
@@ -6,3 +6,64 @@ $('.bs-sidebar').mouseenter(function(){
 $('.bs-sidebar').mouseleave(function(){
 	$(this).animate({width: '40px'}, 200);
 });
+
+//auth logic
+
+var stateKey = 'spotify_auth_state';
+
+/**
+ * Obtains parameters from the hash of the URL
+ * @return Object
+ */
+function getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while (e = r.exec(q)) {
+        hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+}
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+function generateRandomString(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+var params = getHashParams();
+var access_token = params.access_token,
+    state = params.state,
+    storedState = localStorage.getItem(stateKey);
+
+
+loginBtn = $('#login-button');
+
+loginBtn.click(function() {
+    var client_id = 'c9ce30f810254abfa32846f44b5533cf'; // Your client id
+    var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+
+    var state = generateRandomString(16);
+
+    localStorage.setItem(stateKey, state);
+    var scope = 'user-read-private user-read-email user-library-read';
+
+    var url = 'https://accounts.spotify.com/authorize';
+    url += '?response_type=token';
+    url += '&client_id=' + encodeURIComponent(client_id);
+    url += '&scope=' + encodeURIComponent(scope);
+    url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+    url += '&state=' + encodeURIComponent(state);
+
+    window.location = url;
+});
+
