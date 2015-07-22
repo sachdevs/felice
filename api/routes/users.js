263adfa25a7fc033896114c4a40c4db29ba1e350
sachdevs <sachdev.96@gmail.com>
2015-07-22 11:32:02 -0400

    post endpoint users fix

--- api/routes/users.js
+++ api/routes/users.js
@@ -44,10 +44,17 @@ router.post('/', function(req, res) {
     var user = new User();
     user.userId = req.body.userId;
     user.name = req.body.name;
+    user.email = req.body.email;
+    user.spotifyURI = req.body.spotifyURI;
+    user.imageUrl = req.body.imageUrl;
+    user.country = req.body.country;
     user.genreList = req.body.genreList;
+    user.watchingList = req.body.watchingList;
     user.save(function(err) {
-        if (err)
+        if (err){
+            console.log(err);
             return res.status(400).send(err);
+        }
         else
             res.json(req.body);
     });
@@ -67,7 +74,12 @@ router.put('/:userId', function(req, res) {
         var user = users[0];
         user.userId = req.body.userId;
         user.name = req.body.name;
+        user.email = req.body.email;
+        user.spotifyURI = req.body.spotifyURI;
+        user.imageUrl = req.body.imageUrl;
+        user.country = req.body.country;
         user.genreList = req.body.genreList;
+        user.watchingList = req.body.watchingList;
         user.save(function(err) {
             if (err)
                 return res.status(400).send(err);
