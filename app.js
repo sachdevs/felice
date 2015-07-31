aa39ed9a65194a41a937685686226be4f026435d
Saksham Sachdev <sachdev.96@gmail.com>
2015-07-31 11:35:31 -0400

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname + '/servedFiles'));
-app.listen(8888, function() {
-    console.log('listening on 8888');
-});
