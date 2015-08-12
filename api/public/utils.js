e9064d55ff3d35561d537c55d8600930b7064060
sachdevs <sachdev.96@gmail.com>
2015-08-12 11:58:02 -0400

    docs and genre get function

--- api/public/utils.js
+++ api/public/utils.js
@@ -40,7 +40,11 @@ function generateRandomString(length) {
     return text;
 }
 
-
+/**
+ * saves user, tracks, and artist data and populates genres
+ * @param  {obj} data     response from felice api
+ * @return {void}
+ */
 function saveAllDataToDb(data) {
     saveUser(data);
     getTracks(data.access_token, data.local_token, function(songinfo) {
@@ -57,6 +61,11 @@ function saveAllDataToDb(data) {
             (function() {
                 //actually saving tracks to db
                 var track = new Track();
+                //make a list genre list here and put it in artistIdUnique obj
+                //modify save artists to use artistIdUnique to get genreList (thereby minimizing api calls to echonest)
+                //somehow figure out what to do when more than 120 songs need to be gotten genres for
+                //leave when more than 120 songs, check db for the rest
+                //put only if doesnt exist is probably the better option
                 var trackobj = {
                     name: songinfo[i].track.name,
                     trackId: songinfo[i].track.id,
@@ -86,6 +95,13 @@ function saveAllDataToDb(data) {
     });
 }
 
+/**
+ * Saves artists by a list of id's
+ * @param  {Array} artistArr
+ * @param  {String} spotify_token
+ * @param  {String} local_token   jwt
+ * @return {void}
+ */
 function saveArtists(artistArr, spotify_token, local_token) {
     var urlList = createUrlList(artistArr);
     getArtists(urlList, spotify_token, local_token, function(data) {
@@ -117,6 +133,11 @@ function saveArtists(artistArr, spotify_token, local_token) {
     });
 }
 
+/**
+ * list of urls for artists so that they can be saved individually
+ * @param  {Array} artistArr
+ * @return {void}
+ */
 function createUrlList(artistArr) {
     var urlArr = [];
     var url = 'https://api.spotify.com/v1/artists/?ids=';
@@ -135,6 +156,14 @@ function createUrlList(artistArr) {
     return urlArr;
 }
 
+/**
+ * Using list of urls, creates list of artist onjects
+ * @param  {Array}   urlList
+ * @param  {String}   spotify_token
+ * @param  {String}   local_token
+ * @param  {Function} callback         List of artists
+ * @return {void}
+ */
 function getArtists(urlList, spotify_token, local_token, callback) {
     var list = [];
     for (var i = 0; i < urlList.length; i++) {
@@ -170,22 +199,11 @@ function getTracks(spotify_token, local_token, callback) {
     });
 }
 
-function callSpotify(url, data, access_token, callback) {
-    $.ajax(url, {
-        dataType: 'json',
-        data: data,
-        headers: {
-            'Authorization': 'Bearer ' + access_token
-        },
-        success: function(r) {
-            callback(r);
-        },
-        error: function(r) {
-            callback(null);
-        }
-    });
-}
-
+/**
+ * Saves user
+ * @param  {Object} data      from felice api
+ * @return {void}
+ */
 function saveUser(data) {
     var user = new User();
     var obj = {
@@ -205,6 +223,32 @@ function saveUser(data) {
         },
         error: function(model, error) {
             console.log(error.responseText);
+        }
+    });
+}
+
+function getEchonestGenres(spotify_id, callback) {
+    $.getJSON('http://developer.echonest.com/api/v4/artist/profile?api_key=JWARDUHE5GKDMWFDJ&format=jsonp&id=spotify:artist:' + spotify_id + '&bucket=genre&callback=?', function(res) {
+        var arr = res.response.artist.genres;
+        var ret = [];
+        for(var i = 0; i < arr.length; i++)
+            ret.push(arr[i].name);
+        callback(ret);
+    });
+}
+
+function callSpotify(url, data, access_token, callback) {
+    $.ajax(url, {
+        dataType: 'json',
+        data: data,
+        headers: {
+            'Authorization': 'Bearer ' + access_token
+        },
+        success: function(r) {
+            callback(r);
+        },
+        error: function(r) {
+            callback(null);
         }
     });
 }
