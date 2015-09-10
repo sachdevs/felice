5726ef243e0ea5e84217a8f535d099e41b2c8cc6
sachdevs <sachdev.96@gmail.com>
2015-09-10 17:00:31 -0400

    line continues

--- public/views/line.js
+++ public/views/line.js
@@ -6,8 +6,53 @@ var LineGraph = Backbone.View.extend({
     render: function() {
         var allTracks = JSON.parse(localStorage.getItem("trackData"));
         var dateTrackArr = JSON.parse(localStorage.getItem("songData"));
+        var genreList = Object.keys(JSON.parse(localStorage.getItem("genreArtistMap")));
+        var trackIdDateMap = {};
 
-        
+        for(var i = 0; i < dateTrackArr.length; i++)
+            trackIdDateMap[dateTrackArr[i].trackId] = dateTrackArr[i].date;
+
+        var unformattedDataObj = {};
+        for(var i = 0; i < genreList.length; i++){
+            unformattedDataObj[genreList[i]] = {};
+            for(var j = 0; j < allTracks.length; j++){
+                var date = trackIdDateMap[allTracks[j].trackId].split('T')[0];
+                if(!unformattedDataObj[genreList[i]].hasOwnProperty(date))
+                    unformattedDataObj[genreList[i]][date] = 0;
+                for(var k = 0; k < allTracks[j].genreList.length; k++){
+                    if(allTracks[j].genreList[k] === genreList[i])
+                        unformattedDataObj[genreList[i]][date]++;
+                }
+            }
+        }
+        var formattedDataObj = [];
+        var data = [];
+
+        for(var name in unformattedDataObj){
+            var temp = {};
+            temp.key = name;
+            temp.values = [];
+            var datea = 2001;
+            var tempDataObj = {};
+            for(var date in unformattedDataObj[name]){
+                var tempDateObj = {};
+                tempDateObj.Genre = name;
+                tempDateObj.Date = datea;
+                tempDateObj.Number = unformattedDataObj[name][date];
+                temp.values.push(tempDateObj);
+
+                tempDataObj.Genre = name;
+                tempDataObj.Date = datea;
+                tempDataObj.Number = unformattedDataObj[name][date];
+
+                data.push(tempDataObj);
+                datea++;
+            }
+            formattedDataObj.push(temp);
+        }
+        console.log(data);
+
+        this.draw(data.slice(0,2), formattedDataObj.slice(0,2));
     },
     /**
      * data in the format:
@@ -15,62 +60,8 @@ var LineGraph = Backbone.View.extend({
      * Date: Int in unix time
      * Number: # of tracks added at that day
      */
-    draw: function() {
-        var data = [{
-            "Client": "ABC",
-            "sale": "202",
-            "year": "2000"
-        }, {
-            "Client": "ABC",
-            "sale": "215",
-            "year": "2002"
-        }, {
-            "Client": "ABC",
-            "sale": "179",
-            "year": "2004"
-        }, {
-            "Client": "ABC",
-            "sale": "199",
-            "year": "2006"
-        }, {
-            "Client": "ABC",
-            "sale": "134",
-            "year": "2008"
-        }, {
-            "Client": "ABC",
-            "sale": "176",
-            "year": "2010"
-        }, {
-            "Client": "XYZ",
-            "sale": "100",
-            "year": "2000"
-        }, {
-            "Client": "XYZ",
-            "sale": "215",
-            "year": "2002"
-        }, {
-            "Client": "XYZ",
-            "sale": "179",
-            "year": "2004"
-        }, {
-            "Client": "XYZ",
-            "sale": "199",
-            "year": "2006"
-        }, {
-            "Client": "XYZ",
-            "sale": "134",
-            "year": "2008"
-        }, {
-            "Client": "XYZ",
-            "sale": "176",
-            "year": "2013"
-        }];
-        var dataGroup = d3.nest()
-            .key(function(d) {
-                return d.Client;
-            })
-            .entries(data);
-        console.log(JSON.stringify(dataGroup));
+    draw: function(data, dataGroup) {
+        console.log(dataGroup);
         var color = d3.scale.category10();
         this.$el.append('<svg id="visualisation" width="1000" height="500"></svg>');
         var vis = d3.select("#visualisation"),
@@ -84,14 +75,14 @@ var LineGraph = Backbone.View.extend({
             },
             lSpace = WIDTH / dataGroup.length;
         xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([d3.min(data, function(d) {
-                return d.year;
+                return d.Date;
             }), d3.max(data, function(d) {
-                return d.year;
+                return d.Date;
             })]),
             yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([d3.min(data, function(d) {
-                return d.sale;
+                return d.Number;
             }), d3.max(data, function(d) {
-                return d.sale;
+                return d.Number;
             })]),
             xAxis = d3.svg.axis()
             .scale(xScale),
@@ -110,10 +101,10 @@ var LineGraph = Backbone.View.extend({
 
         var lineGen = d3.svg.line()
             .x(function(d) {
-                return xScale(d.year);
+                return xScale(d.Date);
             })
             .y(function(d) {
-                return yScale(d.sale);
+                return yScale(d.Number);
             })
             .interpolate("basis");
         dataGroup.forEach(function(d, i) {
