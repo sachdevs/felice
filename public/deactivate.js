f99c82e8b6c64fbaa737779283c1a2ec745ea162
sachdevs <sachdev.96@gmail.com>
2015-09-28 00:37:06 -0400

    fixed deactivation issue

--- public/deactivate.js
+++ public/deactivate.js
@@ -1,16 +1,17 @@
-$('.deactivate').click(function(){
-    userId = localStorage.getItem("userId");
-    $.ajax(root + '/api/users/' + userId, {
-        dataType: 'json',
-        method: "DELETE",
-        headers: {
-            'x-access-token': localStorage.getItem('local_token')
-        },
-        success: function(r) {
-            localStorage.clear();
-            window.location = root + '/#login';
-        },
-        error: function(r) {
-        }
+$(document).ready(function() {
+    $('#deactivate').click(function() {
+        userId = localStorage.getItem("userId");
+        $.ajax(root + '/api/users/' + userId, {
+            dataType: 'json',
+            method: "DELETE",
+            headers: {
+                'x-access-token': localStorage.getItem('local_token')
+            },
+            success: function(r) {
+                localStorage.clear();
+                window.location = root + '/#login';
+            },
+            error: function(r) {}
+        });
     });
 });
